a5bf0803e0746565e8c5306b056a72d36d1f8e73
Techcraft7 <mctechcraft7@gmail.com>
2020-02-15 19:45:21 -0500

    working on the parser :)

---
+++ 7Sharp/Intrerpreter/7sFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Sharp.Intrerpreter
+{
+    using static Techcraft7_DLL_Pack.Text.ColorConsoleMethods;
+    using static ConsoleColor;
+    using static Console;
+    public class _7sFunction
+    {
+        public int NumberOfArguments;
+        public string Name;
+        public dynamic Code;
+
+        public _7sFunction(string name, int args, Delegate action)
+        {
+            Name = name;
+            NumberOfArguments = args;
+            Code = action;
+        }
+
+        public void Run(out dynamic ReturnValue, params object[] args)
+        {
+            ReturnValue = null;
+            try
+            {
+                ReturnValue = Code.DynamicInvoke(args);
+            }
+            catch (Exception e)
+            {
+                WriteLineColor($"Error in function {Name}: {e.Message}", Red);
+            }
+        }
+    }
+}
---
+++ 7Sharp/Intrerpreter/Sysfunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Sharp.Intrerpreter
+{
+    using static _7Sharp.Program;
+    using static Console;
+    internal static class SystemFunctions
+    {
+        internal static void Init()
+        {
+            interpreter.functions.Add("write", new _7sFunction("write", 1, new Action<dynamic>(Write)));
+        }
+
+        static void Write(dynamic s)
+        {
+            WriteLine(s);
+        }
+    }
+}
