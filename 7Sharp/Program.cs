4f9c5d4f8875b118a5112879642a25c20a57efc3
Techcraft7 <mctechcraft7@gmail.com>
2023-03-15 09:23:05 -0400

    begin creating CLI

--- 7Sharp/Program.cs
+++ 7Sharp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("TODO");
+﻿while (true)
+{
+	WritePrompt();
+	string? line = Console.ReadLine();
+	if (line is null || line.StartsWith("exit"))
+	{
+		break;
+	}
+}
+
+static void WritePrompt()
+{
+	Console.ForegroundColor = ConsoleColor.Green;
+	Console.Write("7Sharp");
+	Console.ForegroundColor = ConsoleColor.Yellow;
+	Console.Write("> ");
+	Console.ForegroundColor = ConsoleColor.White;
+}
